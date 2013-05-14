162fa40ad0f11ce4d0a66616760d464f2a726ce2
encima <encima@gmail.com>
2013-05-14 15:26:44 +0000

    fixed hiding/showing text on clicks

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
+    $('.page p').hide(); 
   $('.page').click(function() {
     id = $(this).attr('id');
     $(this).toggleClass('active');
+    $('.active').css('min-height', '600px');
+    $('.page p').hide();
+    if(!$('#' + id + 'p').is(":visible") && $(this).hasClass('active')) $('#' + id + ' p').show();
     $('.active').css('min-height', '600px');
     $('.active:not(#' + id + ')').removeClass('active');
     $('.page:not(.active)').css('min-height', '50px');
