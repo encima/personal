e6077e6484e62f9a6333863aeed15252c0b53c20
encima@gmail.com <encima@gmail.com>
2013-09-03 10:39:32 +0100

    restructured and added new site for gwillia.ms

---
+++ gwilliams/static/js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $('.img-text h2').hide();
+});
+
+$(".left-nav").each(function() {
+	$(this).data("standardWidth", $(this).width());
+});
+
+$(".left-nav").hover(function() {
+	$(this).stop(true, false).animate({
+	    width: "15%"
+	}, 500, function() {
+		$('.img-text h3').show();
+	} );
+	
+	},
+	function() {
+		$(this).parent().children().each(function() {
+	    	$(this).stop(true, false).animate({
+	        	width: $(this).data("standardWidth")
+	    	}, 500, function() {
+	    		$('.img-text h3').hide();
+	    	} );
+		});
+	}
+);
